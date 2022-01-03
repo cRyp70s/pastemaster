c04d91e1552006792b8a7728feb97075c9119d61
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-01-02 19:41:28 -0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,10 +1,9 @@
 import os
 
 from app import app, db
-from app.models import Paste, View
 from app.views import *
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "XedneworheofSFDfvbdierhu4e43434()#@@("
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or"XedneworheofSFDfvbdierhu4e43434()#@@("
 db.create_all()
 
 if __name__ == "__main__":
