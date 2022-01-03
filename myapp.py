62779c59252d266b2ae8c91561594971cc9a83e6
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-01-02 19:40:55 -0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,15 +1,10 @@
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
+import os
 
 from app import app, db
 from app.models import Paste, View
 from app.views import *
 
-admin = Admin(name="microblog", template_mode="bootstrap4")
-admin.add_view(ModelView(Paste, db.session))
-admin.add_view(ModelView(View, db.session))
-admin.init_app(app)
-app.config["SECRET_KEY"] = "XSFDfvbdierhu4erhuf9re0ri409fdbvnhkvdbc djcffd./scd3"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "XedneworheofSFDfvbdierhu4e43434()#@@("
 db.create_all()
 
 if __name__ == "__main__":
